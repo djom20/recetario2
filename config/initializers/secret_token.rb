0905c4ef1fdce35b15ae76d5313eee25226ed9c2
Ing. Jonathan Olier <djom202@gmail.com>
2013-02-07 11:16:25 -0500

    primer repositorio vacio
    se creo una app en rails vacia para empezar el proyecto.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recetario2::Application.config.secret_token = '98a7fbb371c6ccb1591b45376a9f65ebbbf3c0d37ab2d3e4bb03ff655320e121c14b10bac752e1cf718190cf8890b4867eaa71e9db104c0e186a3bae43df0e16'
